1cc738420ccd299cf61c47ec9acc6e98649ae626
Russell Smith <russ@rainforestqa.com>
2016-02-25 16:10:04 -0800

    Remove junk, fix log messages

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/devicefarm"
@@ -30,20 +29,17 @@ func repeatedlyUpdateDevices() {
 }
 
 func updateDevices() {
-	log.Println("Devices [UPDATING] devices...")
+	log.Println("Devices updating")
 	svc := devicefarm.New(session.New(), &aws.Config{Region: aws.String("us-west-2")})
 	t := time.Now().String()
 
 	params := &devicefarm.ListDevicesInput{}
-	// 	Arn:       aws.String("AmazonResourceName"),
-	// 	NextToken: aws.String("PaginationToken"),
-	// }
 	resp, err := svc.ListDevices(params)
 
 	if err != nil {
 		// Print the error, cast err to awserr.Error to get the Code and
 		// Message from an error.
-		fmt.Println(err.Error())
+		log.Println(err.Error())
 		return
 	}
 
